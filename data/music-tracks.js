cd36e4da98e2f2ef2193f21d9c535f47315c9a4e
astrobionics <msvpost@mail.ru>
2026-04-16 03:20:00 +0300

    сайт обновлен

--- data/music-tracks.js
+++ data/music-tracks.js
@@ -1,17 +1,33 @@
 window.MUSIC_TRACK_BANKS = {
   "easy": [
-    "audio/easy/bach =соната=.mp3",
-    "audio/easy/beethoven =ляля=.mp3",
-    "audio/easy/mozart =лёля=.mp3"
+    "audio/easy/Bach - Air on the G String - BWV 1068 - Arranged for Woodwinds and Strings.mp3",
+    "audio/easy/Bach - Brandenburg Concerto No 3, 1 allegro (Advent Chamber Orchestra).mp3",
+    "audio/easy/Bach - Cello Suite No.1 in G major, BWV 1007 - I. Prelude.mp3",
+    "audio/easy/Bach-Violin-Concerto-No.-2-in-E-Major-1.-Movement-Allegro-BWV-1042.mp3",
+    "audio/easy/Bach+-+Badinerie+-+BWV+1067+-+Arranged+for+Woodwinds+and+Strings.mp3",
+    "audio/easy/Beethoven - Coriolan Overture - Op.62.mp3",
+    "audio/easy/Beethoven - Egmont Overture Op. 84.mp3",
+    "audio/easy/Beethoven - Für Elise - Bagatelle No. 25 in A Minor, WoO 59.mp3",
+    "audio/easy/Beethoven - Moonlight Sonata (3. Movement) - Sonata Nr. 14, Op. 27, Nr. 2 - Arranged for Strings.mp3",
+    "audio/easy/Beethoven - Symphony No. 5 - I. Allegro con brio - Op. 67.mp3",
+    "audio/easy/Beethoven - The Tempest - 3. Movement - Sonata No. 17 in D Minor, Op. 31, No. 2 - Arranged for Strings.mp3",
+    "audio/easy/Beethoven - Violin Sonata No. 9 in A major, Op. 47 - III. Finale. Presto.mp3"
   ],
   "medium": [
-    "audio/medium/bach =соната=.mp3",
-    "audio/medium/beethoven =ляля=.mp3",
-    "audio/medium/mozart =лёля=.mp3"
+    "audio/medium/Bizet, Georges - Carmen Prelude - Arranged for Solo Piano.mp3",
+    "audio/medium/Boccherini - Minuet - Op. 11, No. 5 (G. 275) - Arranged for Piano.mp3",
+    "audio/medium/Bologne, Joseph, Chevalier de Saint-Georges - Sonata No. 3 in G minor - Op. 1a - Arranged for Piano.mp3",
+    "audio/medium/Borodin - String Quartet No. 1 in A Major - I. Moderato - Allegro.mp3",
+    "audio/medium/Brahms - Hungarian Dance No. 2 - Allegro non assai - WoO 1.mp3",
+    "audio/medium/Brahms - Lullaby _ Wiegenlied - Op. 49, No. 4 - Arranged for Music Box (Version 2).mp3"
   ],
   "hard": [
-    "audio/hard/bach =соната=.mp3",
-    "audio/hard/beethoven =ляля=.mp3",
-    "audio/hard/mozart =лёля=.mp3"
+    "audio/hard/Brahms - Symphony No. 1 in C Minor, Op. 68 - I. Un poco sostenuto - Allegro.mp3",
+    "audio/hard/Carreno, Teresa - Gottschalk Waltz - Arranged for Solo Piano.mp3",
+    "audio/hard/Chopin - Etude Op. 10 no. 1 in C major.mp3",
+    "audio/hard/Chopin - Nocturne Op. 9 no. 1 in B-flat minor.mp3",
+    "audio/hard/Grieg - Peer Gynt Suite No. 1, Op. 46 - I. Morning.mp3",
+    "audio/hard/Grieg - Peer Gynt Suite No. 1, Op. 46 - III. Anitra's Dream.mp3",
+    "audio/hard/Haydn - The Seasons - 01 - Der Frühling - Overture And Recit - Seht, Wie Der Strenge Winter Flieht (MIT Concert Choir).mp3"
   ]
 };
